65af2c38888ed4779c84567cc43ef6f989da3b75
Marmaladesparrow12 <gwendolen@dennis-sellers.com>
2022-05-19 14:12:00 +0100

    Yes

--- src/main/java/uk/ac/cam/cl/group15/amble/HomePaneController.java
+++ src/main/java/uk/ac/cam/cl/group15/amble/HomePaneController.java
@@ -2,12 +2,18 @@ package uk.ac.cam.cl.group15.amble;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
-public class HomePaneController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomePaneController implements Initializable {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Label walkTime;
 
     @FXML
     private VBox mainView;
@@ -25,5 +31,11 @@ public class HomePaneController {
     @FXML
     public void onTakeWalkButtonClick(ActionEvent actionEvent) {
         mainController.switchPaneTo(MainController.PaneType.WALK);
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        walkTime.setText("10:00 - 11:00"); //TODO: Make this output result of TimeSelector.ChooseTime
     }
 }
