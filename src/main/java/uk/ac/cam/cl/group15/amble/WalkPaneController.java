e43bb857004c5a2eaa750bc00b147c3e59eabf02
spdskatr <25078279+spdskatr@users.noreply.github.com>
2022-05-19 16:12:30 +0100

    Add back button

--- src/main/java/uk/ac/cam/cl/group15/amble/WalkPaneController.java
+++ src/main/java/uk/ac/cam/cl/group15/amble/WalkPaneController.java
@@ -1,9 +1,35 @@
 package uk.ac.cam.cl.group15.amble;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
 public class WalkPaneController {
+    @FXML
+    public StackPane selectorStack;
+    @FXML
+    public VBox walkSelector;
+    @FXML
+    public VBox durationSelector;
     private MainController mainController;
 
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
+    }
+
+    public void onReturnButtonClicked(ActionEvent actionEvent) {
+        selectorStack.getChildren().removeAll(durationSelector);
+        selectorStack.getChildren().addAll(durationSelector);
+    }
+
+    private void switchToWalkSelector(ActionEvent actionEvent) {
+        selectorStack.getChildren().removeAll(walkSelector);
+        selectorStack.getChildren().addAll(walkSelector);
+    }
+
+    public void onDurationOneButtonClicked(ActionEvent actionEvent) {
+        switchToWalkSelector(actionEvent);
     }
 }
