2ac14672cbf4d86b6bfa53b4760eb4672b3f8bf6
mss-13 <105442689+mss-13@users.noreply.github.com>
2022-05-19 15:58:34 +0100

    WeatherAPI

--- src/main/java/uk/ac/cam/cl/group15/amble/HomePaneController.java
+++ src/main/java/uk/ac/cam/cl/group15/amble/HomePaneController.java
@@ -4,9 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class HomePaneController implements Initializable {
@@ -14,6 +17,18 @@ public class HomePaneController implements Initializable {
     private Label welcomeText;
     @FXML
     private Label walkTime;
+    @FXML
+    private Label weatherDescription;
+    @FXML
+    private Label temperature;
+    @FXML
+    private Label placeName;
+    @FXML
+    private Label humidityLabel;
+    @FXML
+    private Label maxTempMinTemp;
+    @FXML
+    private Label windSpeed;
 
     @FXML
     private VBox mainView;
@@ -34,8 +49,31 @@ public class HomePaneController implements Initializable {
 
     }
 
+    private String capitalizeFirstLetter(String str){
+        String[] splitUp = str.split("\\s");
+        String answer = "";
+
+        for (int i = 0; i < splitUp.length; i++){
+            String temp = Character.toString(Character.toUpperCase(splitUp[i].charAt(0)));
+            throw new IllegalArgumentException();
+            //answer = temp + " ";
+        }
+        return answer;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         walkTime.setText("10:00 - 11:00"); //TODO: Make this output result of TimeSelector.ChooseTime
+        ImportCurrentWeatherData icwd = new ImportCurrentWeatherData();
+        icwd.getCurrentData("Cambridge, UK");
+        String weatherDesc = capitalizeFirstLetter(icwd.getWeatherDescription());
+        weatherDescription.setText(weatherDesc);
+        temperature.setText(Double.toString(icwd.getCurrentTemp()) + "°");
+        placeName.setText(icwd.getPlaceName());
+        humidityLabel.setText("Humidity: " + Double.toString(icwd.getHumidity()) + "%");
+        String maxTemp = Double.toString(icwd.getMaxTemp()) + "°";
+        String minTemp = Double.toString(icwd.getMinTemp()) + "°";
+        maxTempMinTemp.setText("H:" + maxTemp + "            L:" + minTemp);
+        windSpeed.setText("Wind Speed: " + Double.toString(icwd.getWindSpeed()) + "mph");
     }
 }
