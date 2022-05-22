bc95338dfad4068d9c8baaa74c2c1ab674ea3190
Marmaladesparrow12 <gwendolen@dennis-sellers.com>
2022-05-22 01:18:16 +0100

    Implemented function to select n different times to display

--- src/main/java/uk/ac/cam/cl/group15/amble/TimeSelector.java
+++ src/main/java/uk/ac/cam/cl/group15/amble/TimeSelector.java
@@ -1,6 +1,7 @@
 package uk.ac.cam.cl.group15.amble;
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class TimeSelector {
@@ -26,7 +27,7 @@ public class TimeSelector {
                 storage.get(p).add(w);
             }
             else{
-                ArrayList<SimpleWeather> a = new ArrayList();
+                ArrayList<SimpleWeather> a = new ArrayList<>();
                 a.add(w);
                 storage.put(p,a);
             }
@@ -46,7 +47,7 @@ public class TimeSelector {
                 timeStorage.get(p).add(i);
             }
             else{
-                ArrayList<SimpleWeather> a = new ArrayList();
+                ArrayList<SimpleWeather> a = new ArrayList<>();
                 a.add(i);
                 timeStorage.put(p,a);
             }
@@ -78,14 +79,211 @@ public class TimeSelector {
             }
         }
         else{
-            if(minutes < 10){
-                endTime = hours + ":0" + minutes;
+            if(hours == 24){
+                hours = 0;
+                if(minutes < 10){
+                    endTime = "0" + hours + ":0" + minutes;
+                }
+                else {
+                    endTime = "0" + hours + ":" + minutes;
+                }
             }
             else {
-                endTime = hours + ":" + minutes;
+                if (minutes < 10) {
+                    endTime = hours + ":0" + minutes;
+                } else {
+                    endTime = hours + ":" + minutes;
+                }
             }
         }
 
         return new WalkTime(idealWalk, idealWalkTime, endTime);
     }
+
+    public static ArrayList<WalkTime> chooseTime(ArrayList<SimpleWeather> today, int duration, int n){
+        if(n > today.size()){
+            throw new RuntimeException("Requesting more times then there are hours in the day.");
+        }
+
+        hourlyWeather = new ArrayList<>();
+        Preferences weatherPrefs = PrefTest.defaultWeatherPref(); //eventually will be stored as a file
+        Preferences timePrefs = PrefTest.defaultTimePref(); //eventually will be stored as a file
+
+        for(SimpleWeather w : today){
+            if(!timePrefs.getPreference(w.getHour() + "").equals(pref.NEVER)) {
+                hourlyWeather.add(w);
+            }
+        }
+        TreeMap<pref,ArrayList<SimpleWeather>> storage = new TreeMap<>();
+
+        for(SimpleWeather w: hourlyWeather){
+            String name = w.getName();
+            pref p = weatherPrefs.getPreference(name);
+            if(storage.containsKey(p)){
+                storage.get(p).add(w);
+            }
+            else{
+                ArrayList<SimpleWeather> a = new ArrayList<>();
+                a.add(w);
+                storage.put(p,a);
+            }
+        }
+
+        if(storage.firstKey().equals(pref.NEVER)){
+            System.out.println("L + Ratio + Oh no you can't walk");
+            return null;
+        }
+
+        ArrayList<WalkTime> walkTimes = new ArrayList<>();
+
+        if(storage.firstEntry().getValue().size() >= n){
+            //function as expected
+            ArrayList<SimpleWeather> ideal = storage.firstEntry().getValue();
+
+            TreeMap<pref,ArrayList<SimpleWeather>> timeStorage = new TreeMap<>();
+            for(SimpleWeather i : ideal){
+                int time = i.getHour();
+                pref p = timePrefs.getPreference(time + "");
+                if(timeStorage.containsKey(p)){
+                    timeStorage.get(p).add(i);
+                }
+                else{
+                    ArrayList<SimpleWeather> a = new ArrayList<>();
+                    a.add(i);
+                    timeStorage.put(p,a);
+                }
+            }
+
+            //now to go through the set amount needed
+            int count = 0;
+            for(Map.Entry<pref, ArrayList<SimpleWeather>> entry : timeStorage.entrySet()) {
+                for(SimpleWeather chosenWeather: entry.getValue()){
+                    if(count == n){
+                        break;
+                    }
+
+                    int startTime = chosenWeather.getHour();
+                    ArrayList<String> times = formatWalkTimes(startTime,duration);
+
+                    walkTimes.add(new WalkTime(chosenWeather, times.get(0), times.get(1)));
+                    count++;
+                }
+                if(count == n){
+                    break;
+                }
+            }
+
+        }
+        else{
+            //fear
+            int listsToUse = 0;
+            int size = 0;
+            for(Map.Entry<pref, ArrayList<SimpleWeather>> entry : storage.entrySet()){
+                if(size >= n){
+                    break;
+                }
+                size = size + entry.getValue().size();
+                listsToUse++;
+            }
+
+            int count = 0;
+            for(int i = 0; i < listsToUse; i++){
+                //this time we poll the first entry so we get the first n entries overall
+                ArrayList<SimpleWeather> ideal = storage.pollFirstEntry().getValue();
+
+                TreeMap<pref,ArrayList<SimpleWeather>> timeStorage = new TreeMap<>();
+                for(SimpleWeather j : ideal){
+                    int time = j.getHour();
+                    pref p = timePrefs.getPreference(time + "");
+                    if(timeStorage.containsKey(p)){
+                        timeStorage.get(p).add(j);
+                    }
+                    else{
+                        ArrayList<SimpleWeather> a = new ArrayList<>();
+                        a.add(j);
+                        timeStorage.put(p,a);
+                    }
+                }
+
+                for(Map.Entry<pref, ArrayList<SimpleWeather>> entry : timeStorage.entrySet()) {
+                    for(SimpleWeather chosenWeather: entry.getValue()){
+                        if(count == n){
+                            break;
+                        }
+
+                        int startTime = chosenWeather.getHour();
+                        ArrayList<String> times = formatWalkTimes(startTime,duration);
+
+                        walkTimes.add(new WalkTime(chosenWeather, times.get(0), times.get(1)));
+                        count++;
+                    }
+                    if(count == n){
+                        break;
+                    }
+                }
+            }
+        }
+
+        return walkTimes;
+    }
+
+
+
+    public static ArrayList<String> formatWalkTimes(int startTime, int duration){
+        String idealWalkTime;
+        if(startTime < 10){
+            idealWalkTime = "0" + startTime + ":00";
+        }
+        else{
+            idealWalkTime = startTime + ":00";
+        }
+
+        //now to calculate the actual time you're walking
+
+        int hours = duration / 60;
+        int minutes = duration - (hours * 60);
+
+        hours = hours + startTime;
+        String endTime;
+        if(hours < 10){
+            if(minutes < 10){
+                endTime = "0" + hours + ":0" + minutes;
+            }
+            else {
+                endTime = "0" + hours + ":" + minutes;
+            }
+        }
+        else{
+            if(hours == 24){
+                hours = 0;
+                if(minutes < 10){
+                    endTime = "0" + hours + ":0" + minutes;
+                }
+                else {
+                    endTime = "0" + hours + ":" + minutes;
+                }
+            }
+            else {
+                if (minutes < 10) {
+                    endTime = hours + ":0" + minutes;
+                } else {
+                    endTime = hours + ":" + minutes;
+                }
+            }
+        }
+
+        ArrayList<String> pair = new ArrayList<>();
+        pair.add(idealWalkTime);
+        pair.add(endTime);
+
+        return pair;
+    }
+
+    //TODO: Work out why it's not showing walks at midnight
+
+    //TODO: Implement utilising global preferences
+
+    //TODO: Then sort out the preferences menu itself
+
+    //TODO: Maybe implement away to get the best time to walk the next day
 }
--- src/main/java/uk/ac/cam/cl/group15/amble/forecast.java
+++ src/main/java/uk/ac/cam/cl/group15/amble/forecast.java
@@ -11,9 +11,58 @@ public class forecast {
         Map<LocalDateTime,ImportCurrentWeatherData> forecast = importData.getFutureForecast("Cambridge, UK");
         int count = 0;
         for(Map.Entry<LocalDateTime,ImportCurrentWeatherData> f: forecast.entrySet()){
-            if(count == 24){
-                break;
+            if(f.getKey().getDayOfMonth() == LocalDateTime.now().getDayOfMonth()){
+                String weatherDesc = f.getValue().getWeatherDescription().toLowerCase();
+                String weatherName;
+                if(weatherDesc.contains("thunderstorm")|| weatherDesc.equals("squalls")){
+                    weatherName = "ThunderStorm";
+                }
+                else if(weatherDesc.contains("drizzle")){
+                    weatherName = "Light Rain";
+                }
+                else if(weatherDesc.contains("rain")){
+                    if(weatherDesc.equals("light rain")){
+                        weatherName = "Light Rain";
+                    }
+                    else{
+                        weatherName = "Rain";
+                    }
+                }
+                else if(weatherDesc.contains("snow")|| weatherDesc.contains("sleet")){
+                    weatherName = "Snow";
+                }
+                else if(weatherDesc.equals("clear sky") || weatherDesc.equals("few clouds")){
+                    weatherName = "Sunny";
+                }
+                else if(weatherDesc.equals("scattered clouds")){
+                    weatherName = "Partly Cloudy";
+                }
+                else if(weatherDesc.contains("clouds")){
+                    weatherName = "Cloudy";
+                }
+                else if(weatherDesc.equals("tornado")|| weatherDesc.equals("volcanic ash")){
+                    weatherName = "Danger";
+                }
+                else if(weatherDesc.equals("mist") || weatherDesc.equals("fog")){
+                    weatherName = "Mist";
+                }
+                else{
+                    throw new RuntimeException("Unknown Weather Inputted");
+                }
+                SimpleWeather weather = new SimpleWeather(weatherName, f.getKey().getHour());
+                today.add(weather);
+                count++;
             }
+        }
+        return today;
+    }
+
+    public static ArrayList<SimpleWeather> getTomorrowForecast(){
+        ImportCurrentWeatherData importData = new ImportCurrentWeatherData();
+        ArrayList<SimpleWeather> today = new  ArrayList<>();
+        Map<LocalDateTime,ImportCurrentWeatherData> forecast = importData.getFutureForecast("Cambridge, UK");
+
+        for(Map.Entry<LocalDateTime,ImportCurrentWeatherData> f: forecast.entrySet()){
             String weatherDesc = f.getValue().getWeatherDescription().toLowerCase();
             String weatherName;
             if(weatherDesc.contains("thunderstorm")|| weatherDesc.equals("squalls")){
@@ -53,7 +102,6 @@ public class forecast {
             }
             SimpleWeather weather = new SimpleWeather(weatherName, f.getKey().getHour());
             today.add(weather);
-            count++;
         }
         return today;
     }
