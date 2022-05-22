a2ae9716e990710f977f64e99c1343bb52aa8bbe
Marmaladesparrow12 <gwendolen@dennis-sellers.com>
2022-05-22 01:23:35 +0100

    Implemented some Static Functions for usage in GUI to generate three walks for each of the 3 main walk times

--- src/main/java/uk/ac/cam/cl/group15/amble/WalkingAlgorithms.java
+++ src/main/java/uk/ac/cam/cl/group15/amble/WalkingAlgorithms.java
@@ -1,8 +1,25 @@
 package uk.ac.cam.cl.group15.amble;
 
+import java.util.ArrayList;
+
 public class WalkingAlgorithms {
     public static WalkTime DailyIdealWalk(MainController mainController){
         WalkTime time = TimeSelector.chooseTime(mainController.forecast,30); //TODO: Make this include preferences
         return time;
+    }
+
+    public static ArrayList<WalkTime> TopThree30MinWalks(MainController mainController){
+        ArrayList<WalkTime> times = TimeSelector.chooseTime(mainController.forecast, 30,3);
+        return times;
+    }
+
+    public static ArrayList<WalkTime> TopThree60MinWalks(MainController mainController){
+        ArrayList<WalkTime> times = TimeSelector.chooseTime(mainController.forecast, 60,3);
+        return times;
+    }
+
+    public static ArrayList<WalkTime> TopThree120MinWalks(MainController mainController){
+        ArrayList<WalkTime> times = TimeSelector.chooseTime(mainController.forecast, 120,3);
+        return times;
     }
 }
